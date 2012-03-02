a46ff9996b8de1a844af13ed8100db7b9aeabd0b
Bryan Alves <bryanalves@gmail.com>
2012-03-01 21:47:57 -0500

    Solution to problem 7

---
+++ src/007.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+
+import itertools
+
+def euler_7(n):
+    return list(itertools.islice(prime_gen(), n - 1, n))[0];
+
+def prime_gen():
+    n = 2
+    while True:
+        if is_prime(n):
+            yield n
+
+        n += 1
+
+def is_prime(n):
+    if n == 2:
+        return True
+
+    if not n & 1:
+        return False
+
+    for i in range(3, int((n ** 0.5)) + 1, 2):
+        if n % i == 0:
+            return False
+
+    return True
+
+if __name__ == "__main__":
+    print(euler_7(10001))
+
