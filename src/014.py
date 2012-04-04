80286e4a8f05db80156e5cb0ae876ffda4b16f54
Bryan Alves <bryanalves@gmail.com>
2012-04-03 22:44:32 -0400

    Solutions for problem 14

---
+++ src/014.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+"""
+The following iterative sequence is defined for the set of positive integers:
+
+    n → n/2 (n is even)
+    n → 3n + 1 (n is odd)
+
+    Using the rule above and starting with 13, we generate the following sequence:
+        13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+        It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+        Which starting number, under one million, produces the longest chain?
+
+        NOTE: Once the chain starts the terms are allowed to go above one million.
+"""
+
+def euler_14(maxint):
+    arr = [ chaincount2(i) for i in range(1, maxint) ]
+    return arr.index(max(arr)) + 1
+
+def memoize(function):
+    cache = {}
+    def decorated_function(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            val = function(*args)
+            cache[args] = val
+            return val
+    return decorated_function
+
+
+@memoize
+def chaincount(n):
+    """
+    python: 17.253 secs
+    pypy: 14.890 secs
+    """
+    if n == 1:
+        return 1
+
+    if n % 2 == 0:
+        n >>= 1
+    else:
+        n = (3 * n) + 1
+
+    return 1 + chaincount(n)
+
+def chaincount2(n):
+    """
+    python: 1:52.73
+    pypy: 7.6 secs
+    """
+    retval = 1
+
+    while n != 1:
+        if n % 2 == 0:
+            n >>= 1
+        else:
+            n = (3 * n) + 1
+
+        retval += 1
+
+    return retval
+
+if __name__ == "__main__":
+    print(euler_14(1000000))
